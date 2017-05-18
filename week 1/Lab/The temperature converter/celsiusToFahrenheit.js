3796a24ebf26ce9510a3e879ecde8f98e5d19656
jchu521 <jonathanchueh1992@gmail.com>
2017-05-18 17:07:24 +1000

    add

---
+++ week 1/Lab/The temperature converter/celsiusToFahrenheit.js
@@ -0,0 +1,10 @@
+﻿function celsiusToFahrenheit() {
+    var celsius = prompt("Enter celsius temperature");
+    var fahrenheit = celsius * 9 / 5 + 32;
+    document.write(celsius+" C is F" + fahrenheit);
+}
+function fahrenheitToCelsius() {
+    var fahrenheit = prompt("Enter fahrenheit temperature");
+    var celsius = (fahrenheit-32)/1.8;
+    document.write(fahrenheit + " F is C" + celsius);
+}
---
+++ week 2/lab/WebPages-From-Text_Aircraft-100-years-ago/script/xSideControls.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    var xmlhttp;
+    var oe = document.getElementById("menu");
+    
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "sheetMenu.html", false);
+    xmlhttp.send();
+    oe.innerHTML = xmlhttp.responseText;
+};
