1411dc342e3100b24fa9cbe87af90f250ce01d7c
jchu521 <jonathanchueh1992@gmail.com>
2017-05-31 22:38:55 +1000

    update project

---
+++ Project/js/sharedNavbarAndFooter.js
@@ -0,0 +1,30 @@
+﻿window.onload = function () {
+    var xml;
+    //navbar
+    var oe = document.getElementById("navbar");
+
+    xml = new XMLHttpRequest();
+    xml.open("GET", "../html/shared/navbar.html", false);
+    xml.send();
+    oe.innerHTML = xml.responseText;
+    //footer
+    oe = document.getElementById("footer");
+    xml.open("GET", "../html/shared/footer.html", false);
+    xml.send();
+    oe.innerHTML = xml.responseText;
+    /*navbar active*/ 
+    //get the name of the html page
+    var path = window.location.pathname;
+    alert(path);
+    var page = path.split("/").pop();
+    //alert(page);
+    if (page == "index.html") {
+        $("#home").addClass("active");
+    }else if(page == "tourism.html"){
+        $("#tourism").addClass("active");
+    } else if (page == "cuisine.html") {
+        $("#cuisine").addClass("active");
+    } else if (page == "snacks.html") {
+        $("#snacks").addClass("active");
+    }
+}
