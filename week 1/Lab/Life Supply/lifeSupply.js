eeed66fc7659491cee50845c78a22115bccdb8b2
jchu521 <Jonathanchueh1992@gmail.com>
2017-06-14 17:15:46 +1200

    delete all

--- week 1/Lab/Life Supply/lifeSupply.js
+++
@@ -1,9 +0,0 @@
-﻿var maxAge = 99;
-var perDay = 3;
-function total() {
-    var age = prompt("Enter your age");
-    var total = (maxAge - age) * perDay * 365;
-    document.write(("You will need " + total + " to last you until the ripe old age of " + maxAge));
-    //alert("You will need "+total+ " to last you until the ripe old age of " + maxAge);
-}
-window.onload = total;
--- week 2/lab/WebPages-From-Text_Aircraft-100-years-ago/script/xSideControls.js
+++
@@ -1,9 +0,0 @@
-﻿window.onload = function () {
-    var xmlhttp;
-    var oe = document.getElementById("menu");
-    
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "sheetMenu.html", false);
-    xmlhttp.send();
-    oe.innerHTML = xmlhttp.responseText;
-};
