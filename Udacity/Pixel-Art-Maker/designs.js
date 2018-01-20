b2994e0a966692bc85b89eebb05314752e74906f
jonathanchueh1992@gmail.com <jchu521@users.noreply.github.com>
2018-01-21 05:06:28 +1300

    Add reset table

--- Udacity/Pixel-Art-Maker/designs.js
+++ Udacity/Pixel-Art-Maker/designs.js
@@ -12,23 +12,23 @@ document.getElementById('sizePicker').addEventListener('submit', function(e){
 })
 
 const makeGrid = (e) => {
-
-  if(checkGirdSize(gridHeight,gridWidth)){
-    //alert('h: ' + gridHeight.value +',w: ' + gridWidth.value)
-    for(let row=0; row < gridHeight.value; row++){
-      const tableRow = table.insertRow(row);
-      for(let col=0; col < gridWidth.value; col++){
-        tableRow.insertCell(col);
-      }
-    }
-
-    alert('h: ' + gridHeight.value +',w: ' + gridWidth.value)
-  }else{
-    alert("Height and Width must greater than 0")
+  // reset Table
+  while(table.rows.length !== 0) {
+    deleteTableRow();
   }
+
+  for(let row=0; row < gridHeight.value; row++){
+    const tableRow = table.insertRow(row);
+    for(let col=0; col < gridWidth.value; col++){
+      tableRow.insertCell(col);
+    }
+  }
+  alert('h: ' + gridHeight.value +',w: ' + gridWidth.value)
+
 
 }
 
-const checkGirdSize = (gridHeight, gridWidth) => {
-  return (Number(gridWidth.value) > 0 && Number(gridWidth.value) > 0) ? true : false
+//delete table
+const deleteTableRow = () => {
+  table.deleteRow(0);
 }
