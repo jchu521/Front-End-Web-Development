2a83d5f989d1f8d1d38146a6feb33a2e17b12658
jonathanchueh1992@gmail.com <jchu521@users.noreply.github.com>
2018-01-21 04:51:48 +1300

    create Table Row and Col

--- Udacity/Pixel-Art-Maker/designs.js
+++ Udacity/Pixel-Art-Maker/designs.js
@@ -2,9 +2,33 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const gridHeight = document.getElementById('input_height')
+const gridWidth = document.getElementById('input_width')
+const table = document.getElementById('pixel_canvas')
 
-function makeGrid() {
+document.getElementById('sizePicker').addEventListener('submit', function(e){
+  e.preventDefault();
+  makeGrid();
+})
 
-// Your code goes here!
+const makeGrid = (e) => {
 
+  if(checkGirdSize(gridHeight,gridWidth)){
+    //alert('h: ' + gridHeight.value +',w: ' + gridWidth.value)
+    for(let row=0; row < gridHeight.value; row++){
+      const tableRow = table.insertRow(row);
+      for(let col=0; col < gridWidth.value; col++){
+        tableRow.insertCell(col);
+      }
+    }
+
+    alert('h: ' + gridHeight.value +',w: ' + gridWidth.value)
+  }else{
+    alert("Height and Width must greater than 0")
+  }
+
+}
+
+const checkGirdSize = (gridHeight, gridWidth) => {
+  return (Number(gridWidth.value) > 0 && Number(gridWidth.value) > 0) ? true : false
 }
